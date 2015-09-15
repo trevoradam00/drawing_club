cfa1a6c32c22bc18f1a14f46837c123161f16b7d
trevoradam00 <trevorsmith00@gmail.com>
2015-09-15 14:20:20 -0600

    Update migration files

--- db/migrate/20150909084359_create_user.rb
+++ db/migrate/20150909084359_create_user.rb
@@ -3,6 +3,7 @@ class CreateUser < ActiveRecord::Migration
     create_table :users do |t|
         t.string :name
         t.string :email
+        t.string :password_digest #added later
         t.timestamps
     end
   end
